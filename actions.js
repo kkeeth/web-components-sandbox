f589eddff29c420d52dc7140518487d859fe28bd
kkeeth <zensin0082@gmail.com>
2022-05-28 23:54:29 +0900

    feat: added prop

--- actions.js
+++ actions.js
@@ -1,7 +1,22 @@
 class HelloWorld extends HTMLElement {
+  constructor() {
+    super()
+    this.name = 'Web Components!!'
+  }
+
+  static get observedAttributes() {
+    return ['name']
+  }
+
+  attributeChangedCallback(property, oldValue, newValue) {
+    console.log(oldValue, newValue)
+    if (oldValue === newValue) return
+    this[property] = newValue
+  }
+
   // connect component
   connectedCallback() {
-    this.textContent = 'Hello Web Components!!'
+    this.textContent = `Hello ${this.name}`
   }
 }
 customElements.define('hello-world', HelloWorld)
