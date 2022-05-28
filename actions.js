0668fd2a4e58d08cdee04ff48ca31eb82880d3ce
kkeeth <zensin0082@gmail.com>
2022-05-29 08:11:40 +0900

    feat: introducing Shadow DOM sample

--- actions.js
+++ actions.js
@@ -9,13 +9,29 @@ class HelloWorld extends HTMLElement {
   }
 
   attributeChangedCallback(property, oldValue, newValue) {
-    console.log(oldValue, newValue)
     if (oldValue === newValue) return
     this[property] = newValue
+    this.textContent = `Hello ${this[property]}`
   }
 
   // connect component
   connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'closed' })
+
+    shadow.innerHTML = `
+      <style>
+        p {
+          text-align: center;
+          font-weight: normal;
+          padding: 1em;
+          margin: 0 0 2em 0;
+          background-color: #eee;
+          border: 1px solid #666;
+        }
+
+      </style>
+
+      <p>Hello ${this.name}</p>`
     this.textContent = `Hello ${this.name}`
   }
 }
